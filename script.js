61b935ec5a5fa519b4823122ba621ef04fccdf4f
AnnaKhiz <105877365+AnnaKhiz@users.noreply.github.com>
2022-10-03 00:25:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+/*
+1. Реализовать функцию фильтрации isBetween(min, max); 
+Пользователь задает значения min, max с окна ввода. 
+Валидировать значение min, max.
+*/
+let operation, firstDigit, secondDigit, min, max;
+
+function checkMin() {
+	do {
+		min = prompt('Enter min digit');
+		if (isNaN(min)) {
+			alert('It\'s not a digit!');
+		} else if (min == '' || min.match(/^[ ]+$/)) {
+			alert('The field is empty');
+		}
+	} while (isNaN(min) || min == '' || min.match(/^[ ]+$/));
+	return min;
+}
+
+function checkMax() {
+	do {
+		max = prompt('Enter max digit');
+		if (isNaN(max)) {
+			alert('It\'s not a digit!');
+		} else if (max == '' || max.match(/^[ ]+$/)) {
+			alert('The field is empty');
+		}
+	} while (isNaN(max) || max == '' || max.match(/^[ ]+$/));
+	return max;
+}
+
+function isBetween(min, max) {
+	let amount = Number(min);
+	const arr = [amount];
+	return {
+		filterArr() {
+			for (let i = amount; i < max; i++) {
+				arr.push(++amount);
+			}
+			if (+min < +max) {
+				return arr;
+			} else {
+				alert('min must be < max');
+				return 'filter error. try again.';
+			}
+		}
+	}
+}
+//alert(`Result: ${isBetween(checkMin(), checkMax()).filterArr()}`);
+
+/* 
+2. Реализовать функцию calculate(operation)(a)(b). 
+Пользователь указывает нужную ему операцию (+, -, *, /, pow), указывает первый операнд, указывает второй операнд. 
+Все вводимые значения валидировать.
+calculation(pow)(2)(3) => 8.
+*/
+
+function checkFirstDigit() {
+	do {
+		firstDigit = prompt('Enter first digit');
+		if (isNaN(firstDigit)) {
+			alert('It\'s not a digit!');
+		} else if (firstDigit == '' || firstDigit.match(/^[ ]+$/)) {
+			alert('The field is empty');
+		}
+	} while (isNaN(firstDigit) || firstDigit == '' || firstDigit.match(/^[ ]+$/));
+	return firstDigit;
+}
+
+function checkSecondDigit() {
+	do {
+		secondDigit = prompt('Enter second digit');
+		if (isNaN(secondDigit)) {
+			alert('It\'s not a digit!');
+		} else if (secondDigit == '' || secondDigit.match(/^[ ]+$/)) {
+			alert('The field is empty');
+		}
+	} while (isNaN(secondDigit) || secondDigit == '' || secondDigit.match(/^[ ]+$/));
+	return secondDigit;
+}
+
+function checkOperation(operation, firstDigit, secondDigit) {
+	switch (operation.trim()) {
+		case '+':
+			result = +firstDigit + +secondDigit;
+			break;
+		case '-':
+			result = firstDigit - secondDigit;
+			break;
+		case '*':
+			result = firstDigit * secondDigit;
+			break;
+		case '/':
+			if (secondDigit == 0) {
+				alert('Error! Сan\'t be divided by 0!');
+			} else {
+				result = firstDigit / secondDigit;
+			};
+			break;
+		case 'pow':
+			result = Math.pow(firstDigit, secondDigit);
+			break;
+		default:
+			alert('Entered operation is wrong!');
+	}
+	return result;
+}
+
+function calculate(operation) {
+	operation = prompt('Check operation: "+", "-", "*", "/", "pow"');
+	return function (firstDigit) {
+		firstDigit = checkFirstDigit();
+		return function (secondDigit) {
+			secondDigit = checkSecondDigit();
+			return checkOperation(operation, firstDigit, secondDigit);
+		}
+	}
+}
+
+alert(`Result: ${calculate(operation)(firstDigit)(secondDigit)}`);
+
+
+/*
+3. Реализовать функцию сортировки sortByField(fieldName, sortType) для списка товаров с полями name, price, quantity.
+sortType возможные значения: asc, desc - по возрастанию, по убыванию соответственно.
+*/
+
+const products = [
+	{ name: 'Product 1', quantity: 10, price: 25 },
+	{ name: 'Product 2', quantity: 3, price: 55 },
+	{ name: 'Product 3', quantity: 22, price: 35 },
+]
+
+function sortByField(fieldName, sortType) {
+	switch (sortType) {
+		case 'desc':
+			return sortByField(fieldName).desc();
+		case 'asc':
+			return sortByField(fieldName).asc();
+
+	}
+	return {
+		desc() {
+			return products.sort((a, b) => a[fieldName] < b[fieldName] ? 1 : -1);
+		},
+		asc() {
+			return products.sort((a, b) => a[fieldName] > b[fieldName] ? 1 : -1);
+		}
+	}
+}
+
+console.log(sortByField('name', 'desc'));
+

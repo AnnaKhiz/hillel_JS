301c6e723bbb7b98c6b0c00f5e8fb2335147a208
AnnaKhiz <105877365+AnnaKhiz@users.noreply.github.com>
2022-10-21 18:11:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,136 +1,123 @@
-/*
-1. Реализовать функцию фильтрации isBetween(min, max); 
-Пользователь задает значения min, max с окна ввода. 
-Валидировать значение min, max.
-*/
+const inputEmail = document.getElementById('inputEmail');
+const inputPassword = document.getElementById('inputPassword');
+const buttonSubmit = document.getElementById('buttonSubmit');
+const form = document.getElementById('form');
+const errorEmailArea = document.getElementById('errorEmailArea');
+const errorPasArea = document.getElementById('errorPasArea');
+buttonSubmit.disabled = true;
+//console.dir(inputEmail)
 
-let operation, firstDigit, secondDigit, min, max;
+const users = [
+	{ user: 'admin', login: 'admin@gmail.com', password: 'password123' },
+	{ user: 'user', login: 'user@ukr.net', password: 'userpas456' },
+	{ user: 'user_2', login: 'user_user@gmail.com', password: '213password' },
+	{ user: 'user_3', login: 'user.user@gmail.com', password: '215password' }
+];
 
-function checkMinMax() {
-	min = prompt('Enter min digit');
-	max = prompt('Enter max digit');
-	if (isNaN(min)) {
-		alert('It\'s not a digit!');
-	} else if (min == '' || min.match(/^[ ]+$/)) {
-		alert('The field is empty');
-	}
-	if (isNaN(max)) {
-		alert('It\'s not a digit!');
-	} else if (max == '' || max.match(/^[ ]+$/)) {
-		alert('The field is empty');
-	}
-	return [min, max];
+//функция поиска пользователя в "БД"
+function findUser() {
+	users.forEach((element) => {
+		let userLogin = element.login;
+		let userPassword = element.password;
+		console.log(userLogin, userPassword);
+		if (inputEmail.value == userLogin && inputPassword.value == userPassword) {
+			//console.log('yes');
+			errorPasArea.innerHTML = '';
+			submitForm();
+		} else {
+			buttonSubmit.addEventListener('click', (e) => {
+				e.preventDefault();
+				if ((e.target != userPassword || e.target != userLogin) && checkPasLength()) {
+					errorPasArea.innerHTML = '<p class="red-text">Нет такого пользователя!</p>';
+					inputPassword.value = '';
+					inputPassword.style.boxShadow = 'inset 9px -2px 20px 6px rgb(248 58 58 / 20%)';
+				}
+			});
+		}
+	});
+
 }
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+//функция отправки формы
+function submitForm() {
+	buttonSubmit.addEventListener('click', (e) => {
+		e.preventDefault();
+		errorPasArea.innerHTML = '<p class="red-text">Авторизация прошла успешно!</p>';
+		inputPassword.style.boxShadow = 'inset 9px -2px 20px 6px rgb(0 58 58 / 20%)';
+		//inputPassword.value = inputPassword.value;
+		setTimeout(() => {
+			window.location.assign('https://google.com');
+		}, 1000);
+	});
+}
 
-const getEnteredNumbers = checkMinMax();
-function isBetween(min, max) {
-	minNumber = getEnteredNumbers[0];
-	maxNumber = getEnteredNumbers[1];
-	for (let i = 0; i <= arr.length; i++) {
-		if (arr[i] < min && arr[i] > max) {
+
+//событие проверки на пустые поля и пробелы при потере фокуса
+form.addEventListener('focusout', (e) => {
+	console.dir(e.target)
+	if (e.target.tagName != 'input') {
+		if (e.target.value.match(/^[ ]+$/) || e.target.value == '') {
+			e.target.value = '';
+			e.target.style.boxShadow = '';
+			errorPasArea.innerHTML = '<p class="red-text">Заполните все поля!</p>';
+		} else if (e.target.value !== '') {
+			inputPassword.addEventListener('keyup', (e) => {
+				checkPasLength();
+			});
+			notEmptyFields();
+			findUser();
+		} else {
+			buttonSubmit.disabled = true;
+			e.target.style.boxShadow = '';
+		};
+	}
+});
+
+
+//функция валидации (если поля не пустые)
+function notEmptyFields() {
+	checkEmailFormat();
+	if (inputEmail.value != '' && inputPassword.value != '' && checkPasLength() && checkEmailFormat()) {
+		buttonSubmit.disabled = false;
+		errorPasArea.innerHTML = '';
+	} else {
+		buttonSubmit.disabled = true;
+	}
+}
+
+
+//функция проверки длины пароля
+function checkPasLength() {
+	if (inputPassword.value.length >= 6) {
+		errorPasArea.innerHTML = '';
+		inputPassword.style.boxShadow = 'inset 9px -2px 20px 6px rgb(0 58 58 / 20%)';
+		return true;
+	} else if (inputPassword.value.length < 6 && inputPassword.value.length > 0) {
+		console.log('hi');
+		buttonSubmit.disabled = true;
+		errorPasArea.innerHTML = '<p class="red-text">Пароль должен состоять минимум из 6 символов!</p>';
+		inputPassword.style.boxShadow = 'inset 9px -2px 20px 6px rgb(248 58 58 / 20%)';
+		return false;
+	}
+}
+
+//функция валидации емейла
+function checkEmailFormat() {
+	const regex = new RegExp('^[A-Za-z0-9\\._\-]+@+[A-Za-z0-9]+\\.+[A-Za-z]{2,6}$');
+	if ((document.getElementById('inputEmail').value.length) > 1) {
+		if (regex.test(document.getElementById('inputEmail').value)) {
+			inputEmail.style.boxShadow = 'inset 9px -2px 20px 6px rgb(0 58 58 / 20%)';
+			errorEmailArea.innerHTML = '';
+			return true;
+		} else {
+			inputEmail.style.boxShadow = 'inset 9px -2px 20px 6px rgb(248 58 58 / 20%)';
+			errorEmailArea.innerHTML = '<p class="red-text">Email не соответствует формату!</p>';
 			return false;
 		}
-	}
-	return min >= minNumber && max < maxNumber;
-}
-
-console.log(arr.filter(isBetween));
-
-/* 
-2. Реализовать функцию calculate(operation)(a)(b). 
-Пользователь указывает нужную ему операцию (+, -, *, /, pow), указывает первый операнд, указывает второй операнд. 
-Все вводимые значения валидировать.
-calculation(pow)(2)(3) => 8.
-*/
-
-
-function checkDigits() {
-	if (isNaN(firstDigit)) {
-		alert('It\'s not a digit!');
-	} else if (firstDigit == '' || firstDigit.match(/^[ ]+$/)) {
-		alert('The field is empty');
-	}
-	if (isNaN(secondDigit)) {
-		alert('It\'s not a digit!');
-	} else if (secondDigit == '' || secondDigit.match(/^[ ]+$/)) {
-		alert('The field is empty');
-	}
-	return [firstDigit, secondDigit];
-}
-
-
-function checkOperation(operation, firstDigit, secondDigit) {
-	switch (operation.trim()) {
-		case '+':
-			result = +firstDigit + +secondDigit;
-			break;
-		case '-':
-			result = firstDigit - secondDigit;
-			break;
-		case '*':
-			result = firstDigit * secondDigit;
-			break;
-		case '/':
-			if (secondDigit == 0) {
-				alert('Error! Сan\'t be divided by 0!');
-			} else {
-				result = firstDigit / secondDigit;
-			};
-			break;
-		case 'pow':
-			result = Math.pow(firstDigit, secondDigit);
-			break;
-		default:
-			alert('Entered operation is wrong!');
-	}
-	return result;
-}
-
-function calculate(operation) {
-	operation = prompt('Check operation: "+", "-", "*", "/", "pow"');
-	firstDigit = prompt('Enter first digit');
-	secondDigit = prompt('Enter second digit');
-	return function (firstDigit) {
-		let getDigits = checkDigits();
-		firstDigit = getDigits[0];
-		return function (secondDigit) {
-			secondDigit = getDigits[1];
-			return checkOperation(operation, firstDigit, secondDigit);
-		}
+	} else {
+		inputEmail.style.boxShadow = 'inset 9px -2px 20px 6px rgb(248 58 58 / 20%)';
+		errorEmailArea.innerHTML = '<p class="red-text">Email не соответствует формату!</p>';
+		return false;
 	}
 }
 
-//alert(`Result: ${calculate(operation)(firstDigit)(secondDigit)}`);
-
-
-/*
-3. Реализовать функцию сортировки sortByField(fieldName, sortType) для списка товаров с полями name, price, quantity.
-sortType возможные значения: asc, desc - по возрастанию, по убыванию соответственно.
-*/
-
-const products = [
-	{ name: 'Product 1', quantity: 10, price: 25 },
-	{ name: 'Product 2', quantity: 3, price: 55 },
-	{ name: 'Product 3', quantity: 22, price: 35 },
-]
-
-function sortByField(fieldName, sortType) {
-	switch (sortType) {
-		case 'desc':
-			return sortByField(fieldName).desc();
-		case 'asc':
-			return sortByField(fieldName).asc();
-	}
-	return {
-		desc() {
-			return (a, b) => a[fieldName] < b[fieldName] ? 1 : -1;
-		},
-		asc() {
-			return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
-		}
-	}
-}
-
-console.log(products.sort(sortByField('price', 'asc')));

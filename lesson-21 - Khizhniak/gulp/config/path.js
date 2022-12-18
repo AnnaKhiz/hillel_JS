5a9381e6034910fb00beb9ac7df4329fa98237a0
AnnaKhiz <kukurika.anna@gmail.com>
2022-12-18 19:28:41 +0200

    created file path.js

---
+++ lesson-21 - Khizhniak/gulp/config/path.js
@@ -0,0 +1,14 @@
+const buildFolder = './dist';
+const srcFolder = './src';
+
+export const path = {
+    build: {
+        files: `${buildFolder}/files/`,
+    },
+    src: {
+        files: `${srcFolder}/files/`,
+    },
+    watch: {
+        files: `${srcFolder}/files/**/*.*`,
+    },
+}

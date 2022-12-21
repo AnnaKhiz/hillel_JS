a44ce4a98181d0a513d6a0059d379a609555a71f
AnnaKhiz <kukurika.anna@gmail.com>
2022-12-21 22:41:58 +0200

    add html builder

--- lesson-21 - Khizhniak/gulp/tasks/html.js
+++ lesson-21 - Khizhniak/gulp/tasks/html.js
@@ -1,4 +1,7 @@
+const fileInclude = require('gulp-file-include');
+
 module.exports = function copyHtml() {
     return app.gulp.src(app.path.src.html)
+        .pipe(fileInclude())
         .pipe(app.gulp.dest(app.path.build.html));
 }
